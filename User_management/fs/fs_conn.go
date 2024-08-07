f46a75e58bef63c92f2097e60406afeef4d730bb
Tsegay Negassi <tng.nat2023@gmail.com>
2024-08-07 13:17:47 +0300

    fix container service connection to data base cntainers and improve ReadMe file

--- User_management/fs/fs_conn.go
+++ User_management/fs/fs_conn.go
@@ -18,9 +18,9 @@ func Fs_open() {
 	config := map[string]string{
 		"user_name": "postgres", 
 		"db_password": "postgres", 
-		"db_host":     "tele-postgres-user",
-		"db_port":     "5432",
-		"db_name":     "user_db"}
+		"db_host": "localhost", 
+		"db_port": "5432", 
+		"db_name": "task_app"}
 	Destro.Fs_driver_open(config)
 
 }
